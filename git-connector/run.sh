d7d3ebdc88307f7c5f3d381a773032c009b347f6
Mihai Popescu <mihai@unity3d.com>
2019-10-14 13:19:17 +0200

    add git-connector

---
+++ git-connector/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Check the git connector was configured.
+if [ ! -f /opt/perforce/git-connector/gconn.conf ]; then
+
+    echo "Running config..."
+    # Configure a git connecter if not yet.
+    /opt/perforce/git-connector/bin/configure-git-connector.sh -n \
+        --p4port $P4PORT \
+        --super $P4USER \
+        --superpassword $P4PASSWD \
+        --graphdepot $GRAPHDEPOT \
+        --gcuserp4password $GCUSERP4PASSW \
+        --https
+fi
+
+echo "running..."
+# Monitor the log file
+exec ls -1v --color=never /opt/perforce/git-connector/logs/* | xargs tail -f
